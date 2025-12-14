81ecdcf842a0e83c7643a043dce392ae508c26c1
GrigoriyS <smolencevgv@mail.ru>
2025-12-14 12:31:54 +0700

    feat(products): Создал модель товара и зарегистрировал в админке

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+  title = models.CharField(max_length=200, verbose_name="Наименование")
+  text = models.TextField(verbose_name="Описание")
+  price = models.DecimalField(max_digits=15, decimal_places=2, verbose_name='Цена')
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    verbose_name = 'товар'
+    verbose_name_plural = 'товары'
+    db_table = 'shop_products'
+
+  def __str__(self):
+    return self.title
