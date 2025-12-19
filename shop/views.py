7b114a58da61bc96f0f5b1f3462184405eb9ad60
GrigoriyS <smolencevgv@mail.ru>
2025-12-19 15:10:55 +0700

    feat(products): Добавил обработку ошибок  в форму создания товара: - Отображение ошибок в шаблоне - Сделал так чтобы значения полей не сбрасывались при повторном рендере

--- shop/views.py
+++ shop/views.py
@@ -18,6 +18,30 @@ def create_product(request):
     return render(request, 'shop/product_add.html')
   
   if request.method == "POST":
-    product = Product.objects.create(title=request.POST.get('title'), text=request.POST.get('text'), price=request.POST.get('price'))
+    #product = Product.objects.create(title=request.POST.get('title'), text=request.POST.get('text'), price=request.POST.get('price'))
+    title = request.POST.get('title').strip()
+    text = request.POST.get('text').strip()
+    price = request.POST.get('price').strip()
 
-    return redirect('product_detail', product_id=product.id)
+    errors = {}
+
+    if not title:
+      errors['title'] = 'Наименование товара обязательно.'
+    if not text:
+      errors['text'] = 'Описание товара обязательно нужно указать.'
+    if not price:
+      errors['price'] = 'Цену товара обязательно нужно указать.'    
+
+    if not errors:
+      product = Product.objects.create(title=title, text=text, price=price)
+
+      return redirect('product_detail', product_id=product.id)
+    else:
+      context = {
+        'errors': errors,
+        'title': title,
+        'text': text,
+        'price': price
+      }
+
+      return render(request, 'shop/product_add.html', context)
